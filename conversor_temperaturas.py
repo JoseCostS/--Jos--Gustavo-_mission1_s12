637c565a3085bbd16a2ecc4121f06353eff83a7c
JoseCostS <00001159568960sp@al.educacao.sp.gov.br>
2024-06-05 18:51:16 -0300

    Adição de nova função: converter_f_para_c

--- conversor_temperaturas.py
+++ conversor_temperaturas.py
@@ -6,9 +6,10 @@ class ConversorTemperaturas:
         valor_f = valor_c * 1.8 + 32
         return valor_f
     
-    def converter_f_para_c():
-        pass
+    def converter_f_para_c(valor_f):
+        valor_c = (valor_f) - 32 / 1.8
+        return valor_c
     
    
-resultado=ConversorTemperaturas.converter_c_para_f(44)
+resultado=ConversorTemperaturas.converter_f_para_c(40)
 print(resultado)
