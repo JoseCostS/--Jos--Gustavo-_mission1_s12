c5eec43144dbebb30e73bce6829c64c349192805
JoseCostS <00001159568960sp@al.educacao.sp.gov.br>
2024-06-05 18:09:17 -0300

    Refatoração da classe ConversorTemperaturas

--- conversor_temperaturas.py
+++ conversor_temperaturas.py
@@ -2,20 +2,11 @@ class ConversorTemperaturas:
     def __init__(self):
         pass
     
-    def converter_c_para_k():
+    def converter_c_para_f(valor_c):
+        valor_f = valor_c * 1,8 + 32
+        return valor_f
+    
+    def converter_f_para_c():
         pass
     
-    def converter_c_para_f():
-        pass
-    
-    def converter_k_para_f():
-        pass
-    
-    def converter_k_para_c():
-        pass
-
-    def converter_f_para_k():
-        pass
-
-    def converter_f_para_k():
-        pass
+   
