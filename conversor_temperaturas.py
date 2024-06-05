011cb0afea78474b7e1f38df00ab0bd69c3d93b9
JoseCostS <00001159568960sp@al.educacao.sp.gov.br>
2024-06-05 17:50:49 -0300

    Criação da base

---
+++ conversor_temperaturas.py
@@ -0,0 +1,21 @@
+class ConversorTemperaturas:
+    def __init__(self):
+        pass
+    
+    def converter_c_para_k():
+        pass
+    
+    def converter_c_para_f():
+        pass
+    
+    def converter_k_para_f():
+        pass
+    
+    def converter_k_para_c():
+        pass
+
+    def converter_f_para_k():
+        pass
+
+    def converter_f_para_k():
+        pass
