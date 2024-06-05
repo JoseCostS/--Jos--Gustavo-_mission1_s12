c1dfe1708b5a120689a67ff8a908554db4167ddd
JoseCostS <00001159568960sp@al.educacao.sp.gov.br>
2024-06-05 17:30:37 -0300

    Implementação da função: (converter_cm_para_m)

--- conversor_medidas.py
+++ conversor_medidas.py
@@ -1,9 +1,18 @@
 class ConversorMedidas:
     def __init__(self):
       pass
+    
+    def converter_cm_para_m(valor_cm):
+        valor_metros = valor_cm / 100
+        return valor_metros
 
-    def converter_cm_para_m():
-      pass
-
+    
     def converter_m_para_cm():
       pass
+
+# Testing Area
+# Vamos testar e converter 100cm para metros
+resultado = ConversorMedidas.converter_cm_para_m(100)
+
+# O resultado esperando é 1 metro
+print('Resultado: ', resultado)
