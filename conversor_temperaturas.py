a89060105c2989e467d8d6f6b1b8df8b537c2314
JoseCostS <00001159568960sp@al.educacao.sp.gov.br>
2024-06-10 01:40:19 -0300

    Adicionado Menu.

--- conversor_temperaturas.py
+++ conversor_temperaturas.py
@@ -10,6 +10,41 @@ class ConversorTemperaturas:
         valor_f = (valor_c - 32) / 1.8
         return valor_f
     
-resultado=ConversorTemperaturas.converter_f_para_c(40)
-print (resultado)
+def menu():
+ 
+    print("Conversor de Temperaturas")
+    print("1. Celsius para Fahrenheit")
+    print("2. Fahrenheit para Celsius")
+    print("3. Sair")
+    while True:
+        opc=input("escolha a opção desejada: ")
+
+        if opc == '1' :
+       
+            valor_c=float(input('Digite o valor de Celsius: '))
+            resultado=ConversorTemperaturas.converter_c_para_f(valor_c)
+            print("Celsius =",resultado)
+            break
     
+        elif opc == "2" :
+
+            valor_f=float(input('Digite o valor de Fahrenheit: '))
+            resultado=ConversorTemperaturas.converter_f_para_c(valor_f)
+            print("Fahrenheit =",resultado)
+            break
+    
+        elif opc == '3':
+            print("Saindo do conversor...")
+            break
+    
+        else:
+            print("Opção invalida, Tente novamente.")
+            continue
+
+         
+     
+    
+     
+
+if __name__ == "__main__":
+    menu()
