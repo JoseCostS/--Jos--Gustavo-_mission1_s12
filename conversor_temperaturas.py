e7a59716f833bf6e2c75acbb606451d24c151a99
JoseCostS <00001159568960sp@al.educacao.sp.gov.br>
2024-06-10 00:59:45 -0300

    Repaginação do progama Adição da função: converter_c_para_f

--- conversor_temperaturas.py
+++ conversor_temperaturas.py
@@ -2,20 +2,12 @@ class ConversorTemperaturas:
     def __init__(self):
         pass
     
-    def converter_c_para_k():
-        pass
+    def converter_c_para_f(valor_f):
+        valor_c = valor_f * 1.8 + 32
+        return valor_c
     
-    def converter_c_para_f():
+    def converter_f_para_c():
         pass
+resultado=ConversorTemperaturas.converter_c_para_f(15)
+print (resultado)
     
-    def converter_k_para_f():
-        pass
-    
-    def converter_k_para_c():
-        pass
-
-    def converter_f_para_k():
-        pass
-
-    def converter_f_para_k():
-        pass
