2260901e04e487b7080a71260a773182b24e24f0
JoseCostS <00001159568960sp@al.educacao.sp.gov.br>
2024-06-19 18:11:23 -0300

    Reformulação do Progama: Adição do menu principal

--- conversor_medidas.py
+++ conversor_medidas.py
@@ -1,12 +1,9 @@
 class ConversorMedidas:
     def __init__(self):
       pass
-    
     def converter_cm_para_m(valor_cm):
-        valor_metros = valor_cm / 100
-        return valor_metros
-
-    
+        valor_m = valor_cm / 100
+        return valor_m
     def converter_m_para_cm(valor_m):
         valor_cm = valor_m * 100
         return valor_cm
--- conversor_temperaturas.py
+++ conversor_temperaturas.py
@@ -1,15 +1,9 @@
 class ConversorTemperaturas:
     def __init__(self):
         pass
-    
-    def converter_c_para_f(valor_c):
-        valor_f = valor_c * 1.8 + 32
-        return valor_f
-    
-    def converter_f_para_c(valor_f):
-        valor_c = (valor_f) - 32 / 1.8
+    def converter_c_para_f(valor_f):
+        valor_c = valor_f * 1.8 + 32
         return valor_c
-    
-   
-resultado=ConversorTemperaturas.converter_f_para_c(40)
-print(resultado)
+    def converter_f_para_c(valor_c):
+        valor_f = (valor_c - 32) / 1.8
+        return valor_f
